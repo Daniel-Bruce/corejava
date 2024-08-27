ebfe978e4a940c9a343dbeccd9d1595c04f87ca9
Bruce <brucedaniel724@gmail.com>
2024-08-27 09:05:43 +0300

    First type casting example in Casting subfolder

---
+++ Casting/TypeCasting.java
@@ -0,0 +1,17 @@
+package Casting;
+
+public class TypeCasting {
+    public static void main(String[] args) {
+        int num = 129;
+        double digit = num;
+        System.out.println(digit);
+
+       double dig = 123.873;
+       int nu = (int)dig;
+        System.out.println(nu);
+
+        String n = "12345";
+        int u = Integer.parseInt(n);
+        System.out.println(u);
+    }
+}
